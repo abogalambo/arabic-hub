7e05952e2f076d1282761b0d6c23fdce1aab8028
thedecimal <yaser.al3shry@gmail.com>
2015-07-03 15:36:39 +0200

    respond to changes in window.location.hash

--- client/js/modules/router.js
+++ client/js/modules/router.js
@@ -1,9 +1,6 @@
 var page = require('page')
 var quizActions = require('../actions/quiz_actions');
 
-// var assign = require('object-assign');
-// var EventEmitter = require('events').EventEmitter;
-
 var showSlide = function(ctx){
 	quizActions.goToSlide(parseInt(ctx.params.index) - 1);
 }
@@ -25,5 +22,9 @@ var Router = {
 page(routes.index, index);
 page(routes.show_slide, showSlide);
 page({hashbang: true});
+
+window.onhashchange = function(){
+	page(window.location.hash.slice(2));
+}
 
 module.exports = Router;
